b59964485c270bff942df712d546214d66603a86
Sivasothy-Tharsi <sivasothytharsi@gmail.com>
2025-10-25 16:02:56 +0530

    expense form component code modified

--- finance-dashboard/src/components/ExpenseList.js
+++ finance-dashboard/src/components/ExpenseList.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
-const ExpenseList = () => {
+const ExpenseList = ({ expenses }) => {
   return (
-    <div>ExpenseList</div>
-  )
-}
+    <div className="border rounded p-4">
+      <h2 className="text-xl font-bold mb-2">Expenses</h2>
+      {expenses.length === 0 ? (
+        <p>No expenses added yet.</p>
+      ) : (
+        <ul>
+          {expenses.map((expense, index) => (
+            <li
+              key={index}
+              className="flex justify-between border-b py-2 last:border-b-0"
+            >
+              <span>{expense.category}</span>
+              <span>${expense.amount}</span>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
-export default ExpenseList
+export default ExpenseList;

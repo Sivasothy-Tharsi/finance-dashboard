6a3d434471a3f6bc3cc2e570304cef13836eae80
Sivasothy-Tharsi <sivasothytharsi@gmail.com>
2025-10-25 15:45:17 +0530

    dashboard card code added

--- finance-dashboard/src/components/DashboardCard.js
+++ finance-dashboard/src/components/DashboardCard.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const DashboardCard = () => {
+const DashboardCard = ({ title, amount, color }) => {
   return (
-    <div>DashboardCard</div>
-  )
-}
+    <div className={`p-4 rounded shadow ${color} text-white`}>
+      <h3 className="text-lg">{title}</h3>
+      <p className="text-2xl font-bold">${amount}</p>
+    </div>
+  );
+};
 
-export default DashboardCard
+export default DashboardCard;

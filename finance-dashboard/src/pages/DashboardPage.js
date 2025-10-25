b9a32d8155fd75e25ad55864ab9750e29d2b6a78
Sivasothy-Tharsi <sivasothytharsi@gmail.com>
2025-10-25 15:46:21 +0530

    dashboard page code added

--- finance-dashboard/src/pages/DashboardPage.js
+++ finance-dashboard/src/pages/DashboardPage.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const DashboardPage = () => {
-  return (
-    <div>DashboardPage</div>
-  )
-}
+import DashboardCard from "../components/DashboardCard";
 
-export default DashboardPage
+const DashboardPage = () => {
+  const income = 5000;
+  const expenses = 3200;
+  const savings = income - expenses;
+
+  return (
+    <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-4">
+      <DashboardCard title="Income" amount={income} color="bg-green-500" />
+      <DashboardCard title="Expenses" amount={expenses} color="bg-red-500" />
+      <DashboardCard title="Savings" amount={savings} color="bg-blue-500" />
+    </div>
+  );
+};
+
+export default DashboardPage;

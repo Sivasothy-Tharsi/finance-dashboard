b2942a5592f2c48f89abf5cbd15940c7fc627fb2
Sivasothy-Tharsi <sivasothytharsi@gmail.com>
2025-10-25 16:55:29 +0530

    code updated with backend connection

--- finance-dashboard/src/App.js
+++ finance-dashboard/src/App.js
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ExpensesPage from "./pages/ExpensesPage";
-import ReportsPage from "./pages/ReportsPage";
-import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
--- finance-dashboard/src/components/Navbar.js
+++ finance-dashboard/src/components/Navbar.js
@@ -5,8 +5,8 @@ const Navbar = () => {
   return (
   <nav className="p-4 bg-gray-800 text-white flex gap-4">
     <Link to="/">Dashboard</Link>
-    <Link to="/expenses">Expenses</Link>
-    <Link to="/report">Reports</Link>
+    {/* <Link to="/expenses">Expenses</Link>
+    <Link to="/report">Reports</Link> */}
   </nav>
   )
 }
